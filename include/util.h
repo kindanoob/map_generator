3ef0824a0017af26b13aefe1684d930928b7a9ff
kindanoob <evgpavlov1@gmail.com>
2018-02-16 23:00:57 +0500

    change functions naming to CamelCase

--- include/util.h
+++ include/util.h
@@ -5,12 +5,12 @@
 #include <utility>
 
 namespace Util {
-    double dist_squared_from_point_to_point(double x0, double y0, double x1, double y1);
-    double dist_from_point_to_point(double x0, double y0, double x1, double y1);
-    double dist_from_point_to_line(double point_x, double point_y,
+    double DistSquaredFromPointToPoint(double x0, double y0, double x1, double y1);
+    double DistFromPointToPoint(double x0, double y0, double x1, double y1);
+    double DistFromPointToLine(double point_x, double point_y,
                                    double line_a, double line_b, double line_c);
 
-    bool cmp_tile_connected_components_by_size_descending(std::vector<std::pair<int, int> >& cc1,
+    bool CmpTileConnectedComponentsBySizeDescending(std::vector<std::pair<int, int> >& cc1,
                                            std::vector<std::pair<int, int> >& cc2);
 }
 
